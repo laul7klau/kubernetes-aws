d9ad55786d701ad7539e80b11ce12503032ef646
Kin <41984006+laul7klau@users.noreply.github.com>
2021-04-25 08:05:47 +0800

    Add files via upload

---
+++ nginx/create-ingress.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+git clone https://github.com/nginxinc/kubernetes-ingress/
+cd kubernetes-ingress/deployments
+git checkout v1.11.1
+#Set up files
+cp ../examples/complete-example/cafe.yaml .
+cp ../examples/complete-example/cafe-secret.yaml .
+wget https://raw.githubusercontent.com/laul7klau/kubernetes-aws/main/nginx/cafe-ingress.yaml
+wget https://raw.githubusercontent.com/laul7klau/kubernetes-aws/main/nginx/nginx-config.yaml
+#Install Ingress Controller per https://docs.nginx.com/nginx-ingress-controller/installation/installation-with-manifests/
+kubectl apply -f common/ns-and-sa.yaml
+kubectl apply -f rbac/rbac.yaml
+
+kubectl apply -f common/default-server-secret.yaml
+kubectl apply -f common/nginx-config.yaml
+kubectl apply -f common/ingress-class.yaml
+
+kubectl apply -f common/crds/k8s.nginx.org_virtualservers.yaml
+kubectl apply -f common/crds/k8s.nginx.org_virtualserverroutes.yaml
+kubectl apply -f common/crds/k8s.nginx.org_transportservers.yaml
+kubectl apply -f common/crds/k8s.nginx.org_policies.yaml
+kubectl apply -f common/crds/k8s.nginx.org_globalconfigurations.yaml
+kubectl apply -f common/global-configuration.yaml
+#Install Ingress Controller pods as daemonset
+kubectl apply -f daemon-set/nginx-ingress.yaml
+#Exposing service as nodeport and creating AWS loadbalancer
+kubectl create -f service/nodeport.yaml
+kubectl apply -f service/loadbalancer-aws-elb.yaml
+kubectl apply -f nginx-config.yaml
+#Configuring coffee tea app by configuring Ingress Controller through ingress resource
+kubectl create -f cafe.yaml
+kubectl create -f cafe-secret.yaml
+kubectl create -f cafe-ingress.yaml
+
+kubectl get svc -n nginx-ingress
+echo "Run nslookup <hostname>.us-west-2.elb.amazonaws.com to get public IP address of AWS LB."
