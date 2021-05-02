91641743dce3c27bb5cb287c99bee959b9fe9266
Kin <41984006+laul7klau@users.noreply.github.com>
2021-05-02 21:14:04 +0800

    Update create-nginx-ingress.sh

--- bigip-ctrl-ingress/ingressLink/nodeport/create-nginx-ingress.sh
+++ bigip-ctrl-ingress/ingressLink/nodeport/create-nginx-ingress.sh
@@ -13,7 +13,6 @@ kubectl apply -f common/ns-and-sa.yaml
 kubectl apply -f rbac/rbac.yaml
 
 kubectl apply -f common/default-server-secret.yaml
-kubectl apply -f common/nginx-config.yaml
 kubectl apply -f common/ingress-class.yaml
 
 kubectl apply -f common/crds/k8s.nginx.org_virtualservers.yaml
